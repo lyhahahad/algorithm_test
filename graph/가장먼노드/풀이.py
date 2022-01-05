80ed69f5f997c98f9093fc8033134fb015935979
lyhahahad <96465753+lyhahahad@users.noreply.github.com>
2022-01-05 11:55:19 +0900

    Add files via upload

--- graph/가장먼노드/풀이.py
+++ graph/가장먼노드/풀이.py
@@ -1,53 +1,57 @@
 def solution(n, edge):
     answer = 0
-    root = 1
+    root = [1]
     queue = []
     visited_node = []
     enqueue(queue,root)
-    while(not queue_isEmpty(queue)):
+    while(queue_isNotEmpty(queue)):
         visiting_node = dequeue(queue)
-        enqueue(queue, childs(edge, visiting_node, visited_node))
-        visited_node.append(visiting_node)
         print(visiting_node)
-        print(visited_node)
-        if(childsnodenum(edge, visiting_node, visited_node)==0):
+        enqueue(queue, childs(edge, visiting_node, visited_node,queue))
+        print(queue)
+        visited_node.append(visiting_node)
+        if(childsnodenum(edge, visiting_node, visited_node,queue)==0):
+            print(1111111)
             answer+=1
         elif(answer>=1) : 
             answer=0
-        print(answer)
+        # break
     return answer
 
 def enqueue(queue,i):
-    queue.append(i)
+    queue.extend(i)
     return
 
 def dequeue(queue):
     return(queue.pop(0))
 
-def queue_isEmpty(queue):
+def queue_isNotEmpty(queue):
     if(len(queue)==0):
-        return True
-    return False
+        return False
+    return True
 
-def childs(edge, visiting_node, visited_node):
+def childs(edge, visiting_node, visited_node,queue):
     child_list=[]
     for i in edge:
         if (visiting_node in i):
             for j in i:
-                if(visiting_node != j):
+                if(visiting_node != j and j not in visited_node and j not in queue):
                     child_list.append(j)
     return child_list
 
-def childsnodenum(edge, visiting_node, visited_node):
+def childsnodenum(edge, visiting_node, visited_node,queue):
     result = 0
     for i in edge:
         if (visiting_node in i):
             for j in i:
-                if(visiting_node != j):
+                if(visiting_node != j and j not in visited_node and j not in queue):
                     result +=1
     return result
 
 
 n=6
 edge = 	[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
+# for i in range(0,8):
+#     print(edge.pop(0))
+# print(edge)
 print(solution(n,edge))
