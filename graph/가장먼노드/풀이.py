853a94ae38dfa46330668220002e098d40c3065c
lyhahahad <96465753+lyhahahad@users.noreply.github.com>
2022-01-09 16:40:02 +0900

    Add files via upload

--- graph/가장먼노드/풀이.py
+++ graph/가장먼노드/풀이.py
@@ -6,15 +6,16 @@ def solution(n, edge):
     enqueue(queue,root)
     while(queue_isNotEmpty(queue)):
         visiting_node = dequeue(queue)
-        print(visiting_node)
         enqueue(queue, childs(edge, visiting_node, visited_node,queue))
-        print(queue)
         visited_node.append(visiting_node)
-        if(childsnodenum(edge, visiting_node, visited_node,queue)==0):
-            print(1111111)
+        if(childsnodenum(edge, visiting_node, visited_node)==0):
             answer+=1
         elif(answer>=1) : 
             answer=0
+        print("visiting_node : "+str(visiting_node))
+        print("visited_node : "+str(visited_node))
+        print("queue : "+str(queue))
+        print("answer : "+str(answer))
         # break
     return answer
 
@@ -39,12 +40,12 @@ def childs(edge, visiting_node, visited_node,queue):
                     child_list.append(j)
     return child_list
 
-def childsnodenum(edge, visiting_node, visited_node,queue):
+def childsnodenum(edge, visiting_node, visited_node):
     result = 0
     for i in edge:
         if (visiting_node in i):
             for j in i:
-                if(visiting_node != j and j not in visited_node and j not in queue):
+                if(visiting_node != j and j not in visited_node):
                     result +=1
     return result
 
