bb3cea0f0ad98e8a84bfb2c4414b9f92830d9e3e
lyhahahad <96465753+lyhahahad@users.noreply.github.com>
2022-02-15 13:40:09 +0900

    Add files via upload

--- graph/순위/solution.py
+++ graph/순위/solution.py
@@ -2,32 +2,27 @@ def solution(n, results):
     answer = 0
     win=[set() for i in range(n)]
     lose=[set() for i in range(n)]
-    print(win)
-    print(lose)
-    # seperate win, lose results
-    #o(len(results))
+
     for i in range(n) :
         win[results[i][0]-1].add(results[i][1])
         lose[results[i][1]-1].add(results[i][0])
-    print(win)
-    print(lose)
+
+# 아래 과정은 a->b b->c에서 a->c를 찾을 수 있는 풀이이다.
+# [2,1], [3,2], [4,3], [5,4] 해당 result를 가질 때 오류가 발생한다.
+# 이 부분에 dfs 적용하기.
+# 인접 리스트로 dfs 구현하기.
     for i in range(n):
-        print(i)
         for j in win[i]:
-            print(win[j-1])
-            win[i].union(win[j-1])
+            win[i] = win[i].union(win[j-1])
         for k in lose[i]:
-            print(lose[k-1])
-            lose[i].union(lose[k-1])
+            lose[i] = lose[i].union(lose[k-1])
 
     for i in range(n):
         if(len(win[i].union(lose[i]))==n-1):
-            print(i+1)
             answer +=1
-    print(win)
-    print(lose)
     return answer
 
 #space 
 #time
+
 print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]))
